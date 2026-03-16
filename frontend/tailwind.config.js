d1ab3173a0a431f2789d43b2d8803298f878f650
MohanK-pixel <mohnk007@gmail.com>
2026-03-17 02:06:08 +0530

    login ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Colors for the project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#Ef863E",
+      },
+    },
   },
   plugins: [],
-}
+};
 
